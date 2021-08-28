030013ba75172150926253381c8de3e0f36349b9
yolis <yolisha@gmail.com>
2021-08-28 13:27:24 +0300

    Added support for validation during training by factoring eval

--- main.py
+++ main.py
@@ -16,6 +16,97 @@ import sys
 from torchvision import transforms
 from torchvision.datasets import CelebA
 
+
+def eval(args, config, model):
+    model.eval()
+    # Set the dataset and data loader
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                             std=[0.229, 0.224, 0.225])
+    ])
+    dataset = CelebA(args.dataset_path, split=args.mode,
+                     target_type=["attr", "landmarks", "identity"],
+                     transform=transform,
+                     target_transform=None, download=False)
+    loader_params = {'batch_size': 1,
+                     'shuffle': False,
+                     'num_workers': config.get('n_workers')}
+    dataloader = torch.utils.data.DataLoader(dataset, **loader_params)
+    properties = config.get("properties")
+    mean = np.array([0.485, 0.456, 0.406]).reshape(3, 1, 1)
+    std = np.array([0.229, 0.224, 0.225]).reshape(3, 1, 1)
+    stats = {p: [] for p in properties}
+    stats["time"] = []
+    with torch.no_grad():
+        for batch_idx, (samples, targets) in enumerate(dataloader):
+            attributes = targets[0].transpose(0, 1)
+            landmarks = targets[1]
+            identity = targets[2]
+            targets_dict = {}
+            for i, attr_name in enumerate(dataset.attr_names):
+                targets_dict[attr_name] = attributes[i, :] \
+                    .unsqueeze(1).to(device).to(dtype=torch.float32)
+
+            targets_dict["identity"] = identity.to(device).to(dtype=torch.int64) - 1
+            img_h, img_w = samples.shape[2:]
+            targets_dict["landmarks"] = landmarks.to(device)
+
+            samples = samples.to(device)
+            tic = time.time()
+            outputs = model(samples)
+            toc = time.time()
+
+            img_h = samples.shape[2]
+            img_w = samples.shape[3]
+            proc_outputs = postprocess(outputs, config, (img_h, img_w))
+            stats["time"].append(toc - tic)
+            for property, val in proc_outputs.items():
+                # Handle attributes
+                if property in dataset.attr_names:
+                    if val == targets_dict[property]:
+                        stats[property].append(1)
+                    else:
+                        stats[property].append(0)
+
+            if args.plot:
+                img = samples[0].cpu().numpy()
+                # de-normalize
+                img = (img * std) + mean
+                # reshape
+                img = img.transpose(1, 2, 0)
+                ext_img = np.zeros((img_h, int(img_w * 3), 3)) + 255
+                ext_img[:img_h, :img_w, :] = img
+                plt.imshow(ext_img)
+                # Add text
+                h_offset = 10
+                w_offset = 20
+                for property, val in proc_outputs.items():
+                    # Handle attributes
+                    if property in dataset.attr_names:
+                        answer = "Yes" if val == 1 else "No"
+                        s = property + ": {}".format(answer)
+                        if val == targets_dict[property]:
+                            c = 'green'
+                        else:
+                            c = 'red'
+                        plt.text(img_w + w_offset, h_offset, s, c=c, fontsize='x-small')
+                        if h_offset > img_h - 10:
+                            h_offset = 0
+                            w_offset = 200
+                        h_offset += 10
+                    elif property == "landmarks":
+                        est_points = val[0, :].cpu().numpy().reshape(5, 2)
+                        gt_points = landmarks[0, :].cpu().numpy().reshape(5, 2)
+                        plt.scatter(est_points[:, 0], est_points[:, 1], s=10, c='blue')
+                        plt.scatter(gt_points[:, 0], gt_points[:, 1], s=10, c='red')
+                    elif property == "identity":
+                        print("Ground Truth id: {}, Estimated id: {}".format(identity.numpy()[0], val.cpu().numpy()))
+
+                plt.show()
+    return stats
+
+
 if __name__ == "__main__":
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument("mode", help="train, eval or test")
@@ -162,6 +253,31 @@ if __name__ == "__main__":
             if (epoch % n_freq_checkpoint) == 0 and epoch > 0:
                 torch.save(model.state_dict(), checkpoint_prefix + '_checkpoint-{}.pth'.format(epoch))
 
+            if False:
+                args.mode = "valid"
+                logging.info("Guiding loss through validation dataset evaluation")
+                stats = eval(args, config, model)
+                loss_weights = {}
+                weights_sum = 0.0
+                for property, acc in stats.items():
+                    if property in dataset.attr_names:
+                        mean_acc = np.mean(acc)
+                        logging.info("{}: {}".format(property, mean_acc))
+                        weight = 1-mean_acc
+                        loss_weights[property] = 1 + weight
+                        weights_sum += loss_weights[property]
+                mean_weight = weights_sum/(len(loss_weights))
+                print(mean_weight)
+                for property, acc in stats.items():
+                    if property not in dataset.attr_names:
+                        loss_weights[property] = mean_weight
+
+                criterion.weight_dict = loss_weights
+
+                args.mode = "train"
+                model.train()
+
+
             # Scheduler update
             scheduler.step()
 
@@ -170,102 +286,13 @@ if __name__ == "__main__":
 
     else: # Test or Eval
         # Set to eval mode
-        model.eval()
-
-        # Set the dataset and data loader
-        transform = transforms.Compose([
-                                        transforms.ToTensor(),
-                                        transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                                             std=[0.229, 0.224, 0.225])
-                                        ])
-        dataset = CelebA(args.dataset_path, split=args.mode,
-                         target_type=["attr", "landmarks", "identity"],
-                         transform=transform,
-                         target_transform=None, download=False)
-        loader_params = {'batch_size': 1,
-                         'shuffle': False,
-                         'num_workers': config.get('n_workers')}
-        dataloader = torch.utils.data.DataLoader(dataset, **loader_params)
-
-        stats = {}
-        properties = config.get("properties")
-
-        mean = np.array([0.485, 0.456, 0.406]).reshape(3,1,1)
-        std = np.array([0.229, 0.224, 0.225]).reshape(3,1,1)
-        stats = {p: [] for p in properties}
-        stats["time"] = []
-        with torch.no_grad():
-            for batch_idx, (samples, targets) in enumerate(dataloader):
-                attributes = targets[0].transpose(0, 1)
-                landmarks = targets[1]
-                identity = targets[2]
-                targets_dict = {}
-                for i, attr_name in enumerate(dataset.attr_names):
-                    targets_dict[attr_name] = attributes[i, :] \
-                        .unsqueeze(1).to(device).to(dtype=torch.float32)
-
-                targets_dict["identity"] = identity.to(device).to(dtype=torch.int64)-1
-                img_h, img_w = samples.shape[2:]
-                targets_dict["landmarks"] = landmarks.to(device)
-
-                samples = samples.to(device)
-                tic = time.time()
-                outputs = model(samples)
-                toc = time.time()
-
-                img_h = samples.shape[2]
-                img_w = samples.shape[3]
-                proc_outputs = postprocess(outputs, config, (img_h, img_w))
-                stats["time"].append(toc - tic)
-                for property, val in proc_outputs.items():
-                    # Handle attributes
-                    if property in dataset.attr_names:
-                        if val == targets_dict[property]:
-                            stats[property].append(1)
-                        else:
-                            stats[property].append(0)
-
-                if args.plot:
-                    img = samples[0].cpu().numpy()
-                    # de-normalize
-                    img = (img*std)+mean
-                    # reshape
-                    img = img.transpose(1,2,0)
-                    ext_img = np.zeros((img_h, int(img_w*3),3)) + 255
-                    ext_img[:img_h, :img_w, :] = img
-                    plt.imshow(ext_img)
-                    # Add text
-                    h_offset = 10
-                    w_offset = 20
-                    for property, val in proc_outputs.items():
-                        # Handle attributes
-                        if property in dataset.attr_names:
-                            answer = "Yes" if val == 1 else "No"
-                            s = property + ": {}".format(answer)
-                            if val == targets_dict[property]:
-                                c = 'green'
-                            else:
-                                c = 'red'
-                            plt.text(img_w + w_offset, h_offset, s, c=c, fontsize='x-small')
-                            if h_offset > img_h - 10:
-                                h_offset = 0
-                                w_offset = 200
-                            h_offset += 10
-                        elif property == "landmarks":
-                            est_points = val[0,:].cpu().numpy().reshape(5,2)
-                            gt_points = landmarks[0, :].cpu().numpy().reshape(5,2)
-                            plt.scatter(est_points[:, 0], est_points[:, 1], s=10, c='blue')
-                            plt.scatter(gt_points[:, 0], gt_points[:, 1], s=10, c='red')
-                        elif property == "identity":
-                            print("Ground Truth id: {}, Estimated id: {}".format(identity.numpy()[0], val.cpu().numpy()))
-
-                    plt.show()
-
+        stats = eval(args, config, model)
         for property, vals in stats.items():
             if property == "time":
                 print("Time per image: {}".format(np.mean(vals)))
             else:
                 print("{}: acc {}".format(property, np.mean(vals)))
+
 
 
 
